1bc8ca7b19d1c7243185ac4b44d273ed30798613
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-08-07 11:17:38 -0400

    Delete Account
    This commit features the ability to delete an account by adding a
    remove button in the account-service component and connecting the
    action via removeService function

--- app/components/account-service.js
+++ app/components/account-service.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component'
 
 export default Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  actions: {
+    removeAccount () {
+      return this.sendAction('removeAccount', this.get('account'))
+    }
+  }
 })
--- app/routes/account.js
+++ app/routes/account.js
@@ -3,5 +3,10 @@ import Route from '@ember/routing/route'
 export default Route.extend({
   model () {
     return this.get('store').findAll('account')
+  },
+  actions: {
+    removeAccount (account) {
+      account.destroyRecord()
+    }
   }
 })
