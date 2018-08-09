c921928aefdc2a5d18d8ec4295d89eaa00a834ab
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-08-09 11:03:28 -0400

    User facing flashMessages
    This commit adds user facing flash messages for success and failures
    when the user tries to create, update, or destroy an account or example

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 import RSVP from 'rsvp'
 
 export default Route.extend({
+  flashMessages: service(),
   model () {
     return RSVP.hash({
       accounts: this.get('store').findAll('account'),
@@ -10,13 +12,29 @@ export default Route.extend({
   },
   actions: {
     createAccount (account) {
-      account.save().then(() => this.refresh())
+      account.save()
+      .then(() => this.refresh())
+      .then(() => this.get('flashMessages').success('You added a new account'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem creating your account, please you enter something into each field and try again')
+      })
     },
     removeAccount (account) {
       account.destroyRecord()
+      .then(() => this.get('flashMessages').success('You removed your account'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem removing your account, make sure you are signed in and try again')
+      })
     },
     updateAccount (account) {
       account.save()
+      .then(() => this.get('flashMessages').success('You updated your account'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem updating your account, ensure something was entered into each field and try again')
+      })
     }
   }
 })
--- app/routes/example.js
+++ app/routes/example.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 import RSVP from 'rsvp'
 
 export default Route.extend({
+  flashMessages: service(),
   model () {
     return RSVP.hash({
       examples: this.get('store').findAll('example'),
@@ -10,13 +12,29 @@ export default Route.extend({
   },
   actions: {
     createExample (example) {
-      example.save().then(() => this.refresh())
+      example.save()
+      .then(() => this.refresh())
+      .then(() => this.get('flashMessages').success('You added a new example'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem creating your example, please you enter something into each field and try again')
+      })
     },
     deleteExample (example) {
       example.destroyRecord()
+      .then(() => this.get('flashMessages').success('You removed your example'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem removing your example, make sure you are signed in and try again')
+      })
     },
     updateExample (example) {
       example.save()
+      .then(() => this.get('flashMessages').success('You updated your example'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem updating your example, ensure something was entered into each field and try again')
+      })
     }
   }
 })
