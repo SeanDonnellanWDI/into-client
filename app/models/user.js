b9e46d39b97490a05f32437f467b2cac25864ce6
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-08-09 07:16:52 -0400

    Generate album model
    This commit generates a new album model via
    `ember generate model album artist:string title:string`
    
    The album model doesn't really exist in the backend as it is modelled here

--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,6 @@ import DS from 'ember-data'
 export default DS.Model.extend({
   accounts: DS.hasMany('account'),
   email: DS.attr('string'),
-  examples: DS.hasMany('example')
+  examples: DS.hasMany('example'),
+  albums: DS.hasMany('album')
 })
