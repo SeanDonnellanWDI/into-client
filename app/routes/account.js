9fdb13df5caa4d12bbeba5aec307d6b999181ee3
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-08-30 14:54:24 -0400

    Dynamically build Spotify Auth URL using userID

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,11 +1,15 @@
 import Route from '@ember/routing/route'
 import RSVP from 'rsvp'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
+  auth: service(),
+
   model () {
     return RSVP.hash({
       accounts: this.get('store').findAll('account'),
-      account: this.get('store').createRecord('account', {})
+      account: this.get('store').createRecord('account', {}),
+      userId: this.get('auth.credentials.id')
     })
   },
   actions: {
